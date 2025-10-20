5320d40a781440a99c28cc6728dc1f6fb8ab7390
Bharat Lakhiyani <bharat.lakhiyani@gmail.com>
2025-10-19 22:41:21 -0400

    Delete account_request_02 module from account-requests.tf
    Removed account_request_02 module and its parameters.

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -34,43 +34,6 @@ module "account_request_04" {
   account_customizations_name = "SANDBOX"
 }
 
-
-module "account_request_02" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "blakhiy+aftko2@amazon.com"
-    AccountName               = "bharat aftko2"
-    ManagedOrganizationalUnit = "aft-creations (ou-j65s-yxmsnbe8)" 
-    SSOUserEmail              = "blakhiy@amazon.com"
-    SSOUserFirstName          = "Bharat"
-    SSOUserLastName           = "Lakhiyani"
-  }
-
-  account_tags = {
-    "ABC:Owner"       = "blakhiy@amazon.com"
-    "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Dev"
-    "ABC:CostCenter"  = "123456"
-    "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "102"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "AWS Control Tower Lab"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
-  }
-
-  custom_fields = {
-    custom1 = "a"
-    custom2 = "b"
-  }
-
-  account_customizations_name = "SANDBOX"
-}
-
 module "account_request_03" {
   source = "./modules/aft-account-request"
 
