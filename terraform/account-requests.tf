9146d3ca91c5d177372e5fc124e682bf2e42b8d5
Bharat Lakhiyani <bharat.lakhiyani@gmail.com>
2025-10-19 21:54:20 -0400

    Add account request module with parameters and tags

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -33,3 +33,40 @@ module "account_request_01" {
 
   account_customizations_name = "SANDBOX"
 }
+
+
+module "account_request_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "blakhiy+aftko2@amazon.com"
+    AccountName               = "bharat aftko2"
+    ManagedOrganizationalUnit = "aft-creations" 
+    SSOUserEmail              = "blakhiy@amazon.com"
+    SSOUserFirstName          = "Bharat"
+    SSOUserLastName           = "Lakhiyani"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "blakhiy@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
